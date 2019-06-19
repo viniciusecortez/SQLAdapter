d446176c95f6eae9b12ae93f0427a705e29e5e68
viniciusecortez <viniciusecortez@outlook.com>
2019-06-19 02:39:38 -0300

    Criada implementação para o sqlite

--- src/main/java/io/github/viniciusecortez/sqladapter/structure/SQLite.java
+++ src/main/java/io/github/viniciusecortez/sqladapter/structure/SQLite.java
@@ -3,19 +3,35 @@ package io.github.viniciusecortez.sqladapter.structure;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 /**
  *
  * @author viniciusecortez
  */
-public class SQLite {
+public class SQLite extends SQL{
     
-    public SQLite(){
-        
-        
-        
-        
-        
+    public SQLite(String address) throws Exception{
+        super("sqlite", address);
+    }
+
+    @Override
+    public void exec(String query)  throws SQLException{
+            PreparedStatement ps = super.connect.prepareStatement(query);
+            ps.execute();
+    }
+
+    @Override
+    public ResultSet query(String query)throws SQLException {
+        PreparedStatement ps = super.connect.prepareStatement(query);
+        return ps.executeQuery();
+    }
+
+    @Override
+    public void update(String query) throws SQLException {
+        PreparedStatement ps = super.connect.prepareCall(query);
+        ps.executeUpdate();
     }
     
     
