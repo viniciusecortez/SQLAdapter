f6edf724f71b292cb8bee6e6b587378d335f4f45
viniciusecortez <viniciusecortez@outlook.com>
2019-06-19 02:36:58 -0300

    Criada dependencia do junit

---
+++ src/main/java/io/github/viniciusecortez/sqladapter/structure/SQLite.java
@@ -0,0 +1,22 @@
+
+package io.github.viniciusecortez.sqladapter.structure;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author viniciusecortez
+ */
+public class SQLite {
+    
+    public SQLite(){
+        
+        
+        
+        
+        
+    }
+    
+    
+}
